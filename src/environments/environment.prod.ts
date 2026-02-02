2631a9a084c708aa19cbd472edb9ecf6d3681a5d
Megadur <megadur@gmail.com>
2026-02-02 13:04:30 +0100

    fix: ports missing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://htprzu7ksm5kqmcj.myfritz.net',
-  validationBaseUrl: 'http://htprzu7ksm5kqmcj.myfritz.net',
+  apiBaseUrl: 'http://htprzu7ksm5kqmcj.myfritz.net:5030',
+  validationBaseUrl: 'http://htprzu7ksm5kqmcj.myfritz.net:5031',
 };
