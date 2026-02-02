aaf5e0c67852469e3dbb1a0724a54dd3e4064f82
Megadur <megadur@gmail.com>
2026-02-02 13:42:17 +0100

    fix: https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://htprzu7ksm5kqmcj.myfritz.net:5030',
-  validationBaseUrl: 'http://htprzu7ksm5kqmcj.myfritz.net:5031',
+  apiBaseUrl: 'https://htprzu7ksm5kqmcj.myfritz.net:5030',
+  validationBaseUrl: 'https://htprzu7ksm5kqmcj.myfritz.net:5031',
 };
