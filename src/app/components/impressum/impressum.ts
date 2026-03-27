bfb53f092b35746f23f63a0139274060574e86e1
megadur <megadur@gmail.com>
2026-03-27 08:29:01 +0100

    Create Impressum component

---
+++ src/app/components/impressum/impressum.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-impressum',
+  templateUrl: './impressum.html',
+  styleUrls: ['./impressum.css']
+})
+export class Impressum {}
